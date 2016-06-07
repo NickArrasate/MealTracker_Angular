7b26ba2dd27286e9aee16c602f9508fa82d89ebd
sea413 <seanmp42@gmail.com>
2016-06-07 10:33:27 -0700

    Add close function to edit meals.

--- app/edit-meal.component.ts
+++ app/edit-meal.component.ts
@@ -5,17 +5,21 @@ import { Meal } from './Meal.model';
   selector: 'edit-Meal',
   inputs: ['meal'],
   template: `
-  <div class='edit-form col-sm-4' ng-show="showme">
+  <div class='edit-form col-sm-4' *ngIf='showMe' >
   <h3>Edit Info:</h3>
-  <input [(ngModel)]='meal.name' class = 'form-control'/>
-  <input [(ngModel)]='meal.details' class = 'form-control'/>
-  <input [(ngModel)]='meal.calories' class = 'form-control'/>
-  <button ng-click="showme = !showme">Close</button>
+    <input [(ngModel)]='meal.name' class = 'form-control'/>
+    <input [(ngModel)]='meal.details' class = 'form-control'/>
+    <input [(ngModel)]='meal.calories' class = 'form-control'/>
+    <button (click)="closeDisplay()">Close</button>
   </div>
   `
 })
 
 export class EditMealComponent {
   public meal: Meal;
+  public showMe: boolean = true;
+  closeDisplay(){
+      this.showMe = false;
+  }
 
 }
